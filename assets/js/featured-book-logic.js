521c07f0c8fae4896918d06779d04fb03de87de9
Shazaiib47 <shazaib47@hotmail.com>
2022-06-25 23:53:16 +0100

    created another script file to store functionality for the featured books portion of website using structure and algorithm from book-logic

---
+++ assets/js/featured-book-logic.js
@@ -0,0 +1,28 @@
+const getData = async () => {
+    let res = await (
+        await fetch("https://www.googleapis.com/books/v1/volumes?q=HTML5")
+    ).json();
+const featuredBooks = res.items.slice(8,10);
+    let markup = "";
+    featuredBooks.forEach((obj) => {
+        markup += `
+        <div class="featured-book">
+        <div class="featured-book-cover">
+        <img class="featured-book-img" src="${
+            obj.volumeInfo.imageLinks.thumbnail}" alt"" />
+        </div>
+        <div class="featured-book-content">
+        <h4 class="book-title">${obj.volumeInfo.title}</h4>
+        <h5 class="author">${obj.volumeInfo.authors[0]}</h5>
+        <h6 class="pages">${obj.volumeInfo.pageCount[0]}</h6>
+        <class="little-info">${obj.volumeInfo.description.substr(
+            0, 140
+        )}</p>
+        </div>
+        </div>
+        }
+        `;
+    });
+    document.querySelector(".featured-books").insertAdjacentHTML("beforebegin", markup);
+};
+getData();
