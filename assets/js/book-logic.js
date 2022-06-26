e78db82e1d1fb718cc27872be38ccb5301841f8a
Shazaiib47 <shazaib47@hotmail.com>
2022-06-26 01:40:10 +0100

    removed 0 and apended pages to count pages in books so it displays, instead of undefined

--- assets/js/book-logic.js
+++ assets/js/book-logic.js
@@ -14,7 +14,7 @@ const fetchData = async () => {
         <div class="book-content">
         <h4 class="book-title">${obj.volumeInfo.title}</h4>
         <h5 class="author">${obj.volumeInfo.authors[0]}</h5>
-        <h6 class="pages">${obj.volumeInfo.pageCount[0]}</h6>
+        <h6 class="pages">Pages:${obj.volumeInfo.pageCount}</h6>
         <class="little-info">${obj.volumeInfo.description.substr(
             0, 140
         )}</p>
