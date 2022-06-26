862f634ff300f0e76d9317ccba632234eacd0298
Shazaiib47 <shazaib47@hotmail.com>
2022-06-26 17:50:46 +0100

    used jpkc.com/tools/beautiy to beautify both script files and correct indentation and formatting, and pasting the formatted code back in

--- assets/js/book-logic.js
+++ assets/js/book-logic.js
@@ -1,51 +1,42 @@
 // making api call
 const fetchData = async () => {
-    let res = await (
-      await fetch("https://www.googleapis.com/books/v1/volumes?q=HTML5")
-    ).json();
+    let res = await (await fetch("https://www.googleapis.com/books/v1/volumes?q=HTML5")).json();
     const normalBooks = res.items.slice(0, 8);
     let markup = "";
     // creating markup for each card
     normalBooks.forEach((obj) => {
       markup += `
-          <div id=${obj.volumeInfo.title
-            .split(" ")
-            .join("_")} class="book d-flex">
-          <div class="book-cover">
-          <img class="book-img" src="${
-            obj.volumeInfo.imageLinks.thumbnail
-          }" alt"" />
-          </div>
-          <div class="book-content">
-          <h4 class="book-title">${obj.volumeInfo.title}</h4>
-          <h5 class="author">${obj.volumeInfo.authors[0]}</h5>
-          <h6 class="pages">Pages:${obj.volumeInfo.pageCount}</h6>
-          <p class="little-info">${obj.volumeInfo.description.substr(0, 140)}</p>
-          </div>
-       </div>
-          `;
+            <div id=${obj.volumeInfo.title
+              .split(" ")
+              .join("_")} class="book d-flex">
+            <div class="book-cover">
+            <img class="book-img" src="${
+              obj.volumeInfo.imageLinks.thumbnail
+            }" alt"" />
+            </div>
+            <div class="book-content">
+            <h4 class="book-title">${obj.volumeInfo.title}</h4>
+            <h5 class="author">${obj.volumeInfo.authors[0]}</h5>
+            <h6 class="pages">Pages:${obj.volumeInfo.pageCount}</h6>
+            <p class="little-info">${obj.volumeInfo.description.substr(0, 140)}</p>
+            </div>
+         </div>
+            `;
     });
-  
-     // inserting markup
-     document.querySelector(".books").insertAdjacentHTML("afterbegin", markup);
-     
+    // inserting markup
+    document.querySelector(".books").insertAdjacentHTML("afterbegin", markup);
     // Get the id stored in localstorage
     const selectedItem = localStorage.getItem("selectedItem");
-    
-  
     // After 500 ms (after api call), finding element with the book id that was stored in ls and adding class
     setTimeout(() => {
       document.getElementById(`${selectedItem}`).classList.add("is-selected");
     }, 500);
-  
     let nBooks = document.getElementsByClassName("book");
-  
-    
-    for (let i = 0; i < nBooks.length; i++) {
+    for(let i = 0; i < nBooks.length; i++) {
       nBooks[i].addEventListener("click", () => {
         const classes = nBooks[i].classList;
         // Store the selected book id in local storage
-        if (classes.contains("is-selected")) {
+        if(classes.contains("is-selected")) {
           nBooks[i].classList.remove("is-selected");
           localStorage.removeItem("selectedItem", nBooks[i].id);
         } else {
@@ -56,4 +47,3 @@ const fetchData = async () => {
     }
   };
   fetchData();
-  
--- assets/js/featured-book-logic.js
+++ assets/js/featured-book-logic.js
@@ -1,50 +1,41 @@
 const getData = async () => {
-    let res = await (
-      await fetch("https://www.googleapis.com/books/v1/volumes?q=HTML5")
-    ).json();
+    let res = await (await fetch("https://www.googleapis.com/books/v1/volumes?q=HTML5")).json();
     const featuredBooks = res.items.slice(8, 10);
     let markup = "";
     // creating markup for each card
     featuredBooks.forEach((obj) => {
       markup += `
-          <div id=${obj.volumeInfo.title
-            .split(" ")
-            .join("_")} class="featured-book">
-          <div class="featured-book-cover">
-          <img class="featured-book-img" src="${
-            obj.volumeInfo.imageLinks.thumbnail
-          }" alt"" />
-          </div>
-          <div class="book-content">
-          <h4 class="book-title">${obj.volumeInfo.title}</h4>
-          <h5 class="author">${obj.volumeInfo.authors[0]}</h5>
-          <h6 class="pages">Pages:${obj.volumeInfo.pageCount}</h6>
-          <p class="little-info">${obj.volumeInfo.description.substr(0, 140)}</p>
-          </div>
-       </div>
-          `;
+            <div id=${obj.volumeInfo.title
+              .split(" ")
+              .join("_")} class="featured-book">
+            <div class="featured-book-cover">
+            <img class="featured-book-img" src="${
+              obj.volumeInfo.imageLinks.thumbnail
+            }" alt"" />
+            </div>
+            <div class="book-content">
+            <h4 class="book-title">${obj.volumeInfo.title}</h4>
+            <h5 class="author">${obj.volumeInfo.authors[0]}</h5>
+            <h6 class="pages">Pages:${obj.volumeInfo.pageCount}</h6>
+            <p class="little-info">${obj.volumeInfo.description.substr(0, 140)}</p>
+            </div>
+         </div>
+            `;
     });
-  
-     // inserting markup
+    // inserting markup
     document.querySelector(".featured-books").insertAdjacentHTML("afterbegin", markup);
-     
     // Get the id stored in localstorage
     const selectedItem = localStorage.getItem("selectedItem");
-    
-  
     // After 500 ms (after api call), finding element with the book id that was stored in ls and adding class
     setTimeout(() => {
       document.getElementById(`${selectedItem}`).classList.add("is-selected");
     }, 500);
-  
     let fBooks = document.getElementsByClassName("featured-book");
-  
-    
-    for (let i = 0; i < fBooks.length; i++) {
+    for(let i = 0; i < fBooks.length; i++) {
       fBooks[i].addEventListener("click", () => {
         const classes = fBooks[i].classList;
         // Store the selected book id in local storage
-        if (classes.contains("is-selected")) {
+        if(classes.contains("is-selected")) {
           fBooks[i].classList.remove("is-selected");
           localStorage.removeItem("selectedItem", fBooks[i].id);
         } else {
@@ -55,4 +46,3 @@ const getData = async () => {
     }
   };
   getData();
-  
